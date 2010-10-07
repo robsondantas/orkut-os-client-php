36ec38d792c0bb4746a0eb7ed7a6a7e7bf5624fa
biu.dantas@gmail.com <biu.dantas@gmail.com@1dfd5fdf-edca-61b2-3907-cf96de06faa0>
2010-10-07 20:12:49 +0000

    Small modifications on the lib, in order to fix some oauth problems

--- trunk/lib/external/OAuth.php
+++ trunk/lib/external/OAuth.php
@@ -13,7 +13,7 @@ class OAuthConsumer {/*{{{*/
 
   function __construct($key, $secret, $callback_url=NULL) {/*{{{*/
     $this->key = $key;
-    $this->secret = str_replace(" ","",$secret);
+    $this->secret = $secret;
     $this->callback_url = $callback_url;
   }/*}}}*/
 
