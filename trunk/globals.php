1e021de7eb46721bf1a0013059eae71c863014e9
biu.dantas@gmail.com <biu.dantas@gmail.com@1dfd5fdf-edca-61b2-3907-cf96de06faa0>
2011-01-14 13:26:05 +0000

    Added global variable to support bypassed auth

--- trunk/globals.php
+++ trunk/globals.php
@@ -5,11 +5,14 @@ define('g_timezone', 'America/Sao_Paulo');
 
 // orkut api keys. Get yours on: https://www.google.com/accounts/ManageDomains 
 // more info: http://code.google.com/apis/accounts/docs/RegistrationForWebAppsAuto.html#new
-define('g_orkut_consumer_key', '<<consumer key>>');
-define('g_orkut_consumer_secret', '<<consumer secret>>');
+define('g_orkut_consumer_key', '<consumer_key>');
+define('g_orkut_consumer_secret', '<consumer_secret>');
 
 // set this to determine the virtual directory where captcha.php is located. 
-define('g_captcha_handler', '/orkut-os/captcha.php');
+define('g_captcha_handler', '/os-php/captcha.php');
+
+// set this to test bypassed auth
+define('g_calback_direct_handler', '/os-php/callback_direct.php');
 
 // set include path 
 set_include_path(get_include_path() . PATH_SEPARATOR . g_base_dir.'/lib');
