816843c791247d28e18d070c356a928ff0346669
biu.dantas@gmail.com <biu.dantas@gmail.com@1dfd5fdf-edca-61b2-3907-cf96de06faa0>
2011-02-14 17:10:51 +0000

    modified json section. Now this folder represents all files which answers in json format, using services classes.

---
+++ json/list_scraps.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Copyright 2010 - Robson Dantas <biu.dantas@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+require_once("../auth.php");
+require_once("../services/scrap.php");
+
+class GetOrkutScraps extends Scrap {
+	
+	public function __construct($api) {
+		parent::__construct($api);
+	}
+	  
+	public function execute() {
+        echo json_encode( parent::fetchScraps() );
+    }
+	
+}
+
+//log in
+$orkutApi->login();
+
+// instantiate the class
+$k = new GetOrkutScraps($orkutApi);
+$k->execute();
+
+?>
